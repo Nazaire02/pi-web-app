04f36c87db01cec9012d104fd8ab2b8575518cc7
unknown <nazairendouffou@gmail.com>
2024-03-12 20:16:31 +0000

    tableau de bord

--- src/components/candidatCard.tsx
+++ src/components/candidatCard.tsx
@@ -1,4 +1,4 @@
-export default function ConseilCard(props: any) {
+export default function CandidatCard(props: any) {
     return (
         <div className="conseilCardContainer">
             <h4 style={{ color: 'grey', fontWeight: 'bold', fontSize: 25, flexWrap:'wrap', textAlign:'center', marginBottom:3 }}>{props.title}</h4>
--- src/views/BoardTable.tsx
+++ src/views/BoardTable.tsx
@@ -3,17 +3,34 @@ import CardType1 from "../components/cardType1"
 import ConseilCard from "../components/conseilCard"
 
 export default function BoardTable() {
+    const cardConseil1 = {
+        title: "QUELQUES REGLES ET CONSEILS AVANT DE SE RENDRE DANS LES BUREAUX DE VOTE",
+        items:[
+            "S'informer sur les candidats et les enjeux",
+            "Vérifier leurs inscriptions sur la liste électorale",
+            "Se munir des documents nécessaires"
+        ]
+    };
+
+    const cardConseil2 = {
+        title: "QUELQUES REGLES ET CONSEILS ETANT DE SE RENDRE DANS LES BUREAUX DE VOTE",
+        items:[
+            "S'informer sur les candidats et les enjeux",
+            "Vérifier leurs inscriptions sur la liste électorale",
+            "Se munir des documents nécessaires"
+        ]
+    };
+
     return (
         <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-            <h4>Tableau de bord</h4>
             <div style={{display:'flex', flexDirection:'row'}}>
                 <CardType1 title="Nombre de candidats" number={10}/>
                 <CardType1 title="Nombre d'électeurs" number={20}/>
                 <CardType1 title="Total des votes" number={30}/>
             </div>
             <div style={{display:'flex', flexDirection:'row'}}>
-                <ConseilCard/>
-                <ConseilCard/>
+                <ConseilCard title={cardConseil1.title} items={cardConseil1.items}/>
+                <ConseilCard title={cardConseil2.title} items={cardConseil2.items}/>
             </div>
         </div>
     )
