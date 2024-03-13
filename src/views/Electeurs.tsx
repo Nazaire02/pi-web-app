87950a3342f6dfb5d4f5bd8b1e3f3aefeb6c3538
Nazaire02 <102226213+Nazaire02@users.noreply.github.com>
2024-03-13 10:34:48 +0000

    electeurs et rapports (#4)
    Co-authored-by: lacinatoure0730 <lacinatoure0730@gmail.com>

--- src/views/Electeurs.tsx
+++ src/views/Electeurs.tsx
@@ -1,7 +1,34 @@
+import "../App.css"
 export default function Electeurs(){
     return(
-        <div>
-        <h4>Electeurs</h4>
-        </div>  
+        <><div>
+            <h2>Liste des électeurs</h2> 
+        </div><table>
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Prénoms</th>
+                        <th>Numéro Électeur</th>
+                        <th>Bureau de Vote</th>
+                        <th>Centre de Vote</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>John</td>
+                        <td>Doe</td>
+                        <td>123456789</td>
+                        <td>Bureau A</td>
+                        <td>Centre 1</td>
+                        <td>
+                            <button>Afficher</button>
+                            <button>Modifier</button>
+                            <button>Supprimer</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table></>
+            
     )
 }
